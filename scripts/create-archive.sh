99588393c64209c85e74221602833837eb388192
Bruno Alla <alla.brunoo@gmail.com>
2023-07-17 17:54:09 +0100

    Fix ignoring

--- scripts/create-archive.sh
+++ scripts/create-archive.sh
@@ -7,14 +7,17 @@ DEST=shootime
 sed -i "s/0.0.0/${VERSION}/g" config*.xml shootime.php
 mkdir -p $DEST
 
-find . -not -name "shootime" \
-       -not -name ".gitignore" \
-       -not -name ".pre-commit-config.yaml" \
-       -not -name ".php-cs-fixer.dist.php" \
-       -not -path ".git/*" \
-       -not -path ".github/*" \
-       -not -path ".scripts/*" \
-       -exec cp -t $DEST {} +
+find . \
+  -not -name ".gitignore" \
+  -not -name ".pre-commit-config.yaml" \
+  -not -name ".php-cs-fixer.dist.php" \
+  -not -name ".DS_Store" \
+  -not -path "./shootime*" \
+  -not -path "./.idea*" \
+  -not -path "./.git*" \
+  -not -path "./.github*" \
+  -not -path "./.scripts*" \
+  -exec cp -t $DEST {} +
 
 zip -r "$ARCHIVE_NAME" $DEST
 

31f61cad90be875e4041d6634dbaf8d5e8035102
Bruno Alla <alla.brunoo@gmail.com>
2023-07-17 17:45:08 +0100

    Extract archive creation to a script

---
+++ .github/create-archive.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+ARCHIVE_NAME=$2
+
+sed -i "s/0.0.0/${VERSION}/g" config*.xml shootime.php
+mkdir -p shootime
+
+find . -not -name "shootime" \
+       -not -name ".gitignore" \
+       -not -name ".pre-commit-config.yaml" \
+       -not -name ".php-cs-fixer.dist.php" \
+       -not -path ".git/*" \
+       -not -path ".github/*" \
+       -exec cp -t dest/ {} +
+
+zip -r "$ARCHIVE_NAME" shootime
+
+echo "Created $ARCHIVE_NAME"

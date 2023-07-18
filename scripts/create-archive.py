740a64519b62c85a98cd92769d92efe2c9f54080
Bruno Alla <alla.brunoo@gmail.com>
2023-07-18 13:20:33 +0100

    Add more logging & exclude archive from files to archives

--- scripts/create-archive.py
+++ scripts/create-archive.py
@@ -13,20 +13,25 @@ EXCLUSION_LIST = {
     ".php-cs-fixer.dist.php",
     ".pre-commit-config.yaml",
     ".idea",
+    ".tool-versions",
+    "Dockerfile",
     "scripts",
     "venv",
 }
 
 
 def main(archive_name: str):
+    print(f"Creating file {archive_name}...")
+    exclusion_list = EXCLUSION_LIST | {archive_name}
     with zipfile.ZipFile(archive_name, "w") as zf:
         for file_path in CURRENT_DIRECTORY.glob("**/*"):
             relative_path = file_path.relative_to(CURRENT_DIRECTORY)
             parent_dir = relative_path.parts[0]
-            if parent_dir in EXCLUSION_LIST:
+            if parent_dir in exclusion_list:
                 continue
 
             archive_path = Path("shootime") / relative_path
+            print(f"Adding {archive_path}...")
             zf.write(file_path, arcname=archive_path)
 
     print(f"Zip file created: {archive_name}")
